177c47c1be7f76408cc522cd5d5b701f7b88c25a
Jordi Masip <jordimasipb@gmail.com>
2017-07-29 17:23:58 +0200

    First commit

---
+++ src/app/components/heroes/heroe.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Heroe }  from "../../interfaces/heroe.interface";
+import { HeroesService} from "../../services/heroes.service";
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: []
+})
+export class HeroeComponent implements OnInit {
+
+  heroe:Heroe = {
+    nombre:"",
+    bio:"",
+    casa:"Marvel"
+  }
+
+  nuevo:boolean = false;
+  id:string;
+
+  constructor(private _heroeService:HeroesService,
+              private router:Router,
+              private route:ActivatedRoute) {
+    this.route.params
+        .subscribe(parametros => this.id = parametros['id']);
+  }
+
+  ngOnInit() {
+  }
+
+  guardar() {
+    console.log(this.heroe);
+
+    if(this.id =="nuevo") {
+      this._heroeService.nuevoHeroe( this.heroe)
+            .subscribe( data => {
+              this.router.navigate(['/heroe',data.name]);
+            },
+          error => console.error(error));
+    } else {
+      this._heroeService.actualizarHeroe( this.heroe, this.id)
+            .subscribe( data => {
+              console.log("data");
+            },
+          error => console.error(error));
+    }
+
+  }
+}
---
+++ src/app/services/heroes.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+import { Heroe } from "../interfaces/heroe.interface";
+import 'rxjs/Rx';
+
+@Injectable()
+export class HeroesService {
+
+  heroesURL:string = "https://heroesapp-95d61.firebaseio.com/heroes.json";
+  heroeURL:string = "https://heroesapp-95d61.firebaseio.com/heroes";
+
+  constructor(private http: Http) { }
+
+  nuevoHeroe(heroe:Heroe) {
+    let body = JSON.stringify(heroe);
+    let headers = new Headers({
+      'Content-Type' : 'application-json'
+    });
+
+    return this.http.post( this.heroesURL, body, { headers } )
+            .map( res => {
+              console.log(res.json());
+              return res.json();
+            })
+  }
+
+  actualizarHeroe(heroe:Heroe, key$:string) {
+    let body = JSON.stringify(heroe);
+    let headers = new Headers({
+      'Content-Type' : 'application-json'
+    });
+
+    let url = `${this.heroeURL}/${key$}.json`
+
+    return this.http.put( url, body, { headers } )
+            .map( res => {
+              console.log(res.json());
+              return res.json();
+            })
+  }
+
+}
